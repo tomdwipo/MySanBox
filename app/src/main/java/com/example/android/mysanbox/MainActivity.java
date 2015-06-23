24d064b8408b91376e7b09d1130e3185f9240531
tommyputranto <tommyputranto1@gmail.com>
2015-06-24 00:08:47 +0700

    baru

--- app/src/main/java/com/example/android/mysanbox/MainActivity.java
+++ app/src/main/java/com/example/android/mysanbox/MainActivity.java
@@ -1,16 +1,85 @@
 package com.example.android.mysanbox;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView artistTextView;
+    private Button prevButton;
+    private Button playButton;
+    private Button nextButton;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.game_field);
+
+        prevButton = (Button)findViewById(R.id.prevButton);
+        playButton = (Button)findViewById(R.id.playButton);
+        nextButton = (Button)findViewById(R.id.nextButton);
+        artistTextView = (TextView)findViewById(R.id.ArtistTextView);
+
+        playButton.setOnClickListener(this);
+        prevButton.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.playButton:
+               if (mediaPlayer.isPlaying()){
+                   pauseMusic();
+               }else {
+                   playMusic();
+               }
+
+                break;
+            case R.id.prevButton:
+
+
+                break;
+            case R.id.nextButton:
+
+                break;
+        }
+
+    }
+    public void playMusic(){
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+            artistTextView.setText("Music Playing Now..");
+            playButton.setBackgroundDrawable(getResources().getDrawable(android.R.drawable.ic_media_pause));
+
+        }
+
+    }
+    public void pauseMusic(){
+        if (mediaPlayer !=null){
+            mediaPlayer.pause();
+            artistTextView.setText("Music Pause!");
+            playButton.setBackgroundDrawable(getResources().getDrawable(android.R.drawable.ic_media_play));
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mediaPlayer !=null && mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        super.onDestroy();
     }
 
     @Override
@@ -34,4 +103,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
